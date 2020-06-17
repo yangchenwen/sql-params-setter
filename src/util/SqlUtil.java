6d0576519de5a2f08ef8c261a7d085e9ebc9e766
yangchenwen <ads8528@163.com>
2020-06-17 10:09:47 +0800

    add: sql format optimize

--- src/util/SqlUtil.java
+++ src/util/SqlUtil.java
@@ -23,9 +23,12 @@ public class SqlUtil {
         SQL_FORMAT_KW.put("(?i)from", lineSeparator + "from");
         SQL_FORMAT_KW.put("(?i)where", lineSeparator + "where");
         SQL_FORMAT_KW.put("(?i)and", lineSeparator + "\tand");
+        SQL_FORMAT_KW.put("(?i)or", lineSeparator + "\tor");
         SQL_FORMAT_KW.put("(?i)left join", lineSeparator + "\tleft join");
         SQL_FORMAT_KW.put("(?i)right join", lineSeparator + "\tright join");
         SQL_FORMAT_KW.put("(?i)inner join", lineSeparator + "\tinner join");
+        SQL_FORMAT_KW.put("(?i)order by", lineSeparator + "order by");
+        SQL_FORMAT_KW.put("(?i)group by", lineSeparator + "group by");
     }
 
     public static String format(String sql) {
