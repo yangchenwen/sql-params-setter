da3b45bb4c0012257233674f5a729bf8aa316dcf
yangchenwen <ads8528@163.com>
2020-06-17 11:57:24 +0800

    add: sql format optimize

--- src/util/SqlUtil.java
+++ src/util/SqlUtil.java
@@ -19,16 +19,16 @@ public class SqlUtil {
     static {
         String lineSeparator = System.lineSeparator();
         SQL_FORMAT_KW.put(",", "," + lineSeparator + "\t");
-        SQL_FORMAT_KW.put("(?i)select", "select" + lineSeparator + "\t");
-        SQL_FORMAT_KW.put("(?i)from", lineSeparator + "from");
-        SQL_FORMAT_KW.put("(?i)where", lineSeparator + "where");
-        SQL_FORMAT_KW.put("(?i)and", lineSeparator + "\tand");
-        SQL_FORMAT_KW.put("(?i)or", lineSeparator + "\tor");
-        SQL_FORMAT_KW.put("(?i)left join", lineSeparator + "\tleft join");
-        SQL_FORMAT_KW.put("(?i)right join", lineSeparator + "\tright join");
-        SQL_FORMAT_KW.put("(?i)inner join", lineSeparator + "\tinner join");
-        SQL_FORMAT_KW.put("(?i)order by", lineSeparator + "order by");
-        SQL_FORMAT_KW.put("(?i)group by", lineSeparator + "group by");
+        SQL_FORMAT_KW.put("(?i)\\s+select\\s+", "select" + lineSeparator + "\t");
+        SQL_FORMAT_KW.put("(?i)\\s+from\\s+", lineSeparator + "from ");
+        SQL_FORMAT_KW.put("(?i)\\s+where\\s+", lineSeparator + "where ");
+        SQL_FORMAT_KW.put("(?i)\\s+and\\s+", lineSeparator + "\tand ");
+        SQL_FORMAT_KW.put("(?i)\\s+or\\s+", lineSeparator + "\tor ");
+        SQL_FORMAT_KW.put("(?i)\\s+left join\\s+", lineSeparator + "\tleft join ");
+        SQL_FORMAT_KW.put("(?i)\\s+right join\\s+", lineSeparator + "\tright join ");
+        SQL_FORMAT_KW.put("(?i)\\s+inner join\\s+", lineSeparator + "\tinner join ");
+        SQL_FORMAT_KW.put("(?i)\\s+order by\\s+", lineSeparator + "order by ");
+        SQL_FORMAT_KW.put("(?i)\\s+group by\\s+", lineSeparator + "group by ");
     }
 
     public static String format(String sql) {
