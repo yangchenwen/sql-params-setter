ad24b606a06e1ac520be74f9ab01282696a4f2a0
yangchenwen <ads8528@163.com>
2020-06-19 15:43:21 +0800

    add: icons

---
+++ src/icons/Icons.kt
@@ -0,0 +1,14 @@
+package icons
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+/**
+ * @author yangchenwen
+ * @since 2020-06-19 14:56:09
+ */
+object Icons {
+
+    @JvmField
+    val PluginIcon: Icon = IconLoader.getIcon("/icon/pluginIcon.svg")
+}
